a899faabd9e18e162319f5ecb09654e2bc85538a
Gabe Hummel <s012631@students.lmsd.orrg>
2019-01-09 10:30:17 -0500

    Fixed issue with textfields, imported FirebaseAuth, made a responder chain of text field, buttons not yet implemented

--- Mayday/SignUpViewController.swift
+++ Mayday/SignUpViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpViewController: UIViewController, UITextFieldDelegate {
 
@@ -14,8 +15,13 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var emailAddressTextField: UITextField!
     @IBOutlet weak var passwordConfirmationTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
     
     @IBAction func signUpButtonTouchedUp(_ sender: UIButton) {
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
     }
     
     override func viewDidLoad() {
@@ -24,4 +30,21 @@ class SignUpViewController: UIViewController, UITextFieldDelegate {
         emailAddressTextField.delegate=self
         emailAddressTextField.becomeFirstResponder()
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if emailAddressTextField.isFirstResponder{
+            passwordTextField.becomeFirstResponder()
+        }
+        else if(passwordTextField.isFirstResponder){
+            passwordConfirmationTextField.becomeFirstResponder()
+        }
+        else{
+            self.view.endEditing(true)
+            passwordConfirmationTextField.resignFirstResponder()
+            signUpButton.isEnabled=true
+        }
+        
+        return true
+    }
+    
 }
