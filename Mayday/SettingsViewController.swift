bd39934bba02aa3b7d1ac2600a9352eef2da5c6b
Gabe Hummel <s012631@students.lmsd.orrg>
2019-04-28 17:53:32 -0400

    scroll views added

--- Mayday/SettingsViewController.swift
+++ Mayday/SettingsViewController.swift
@@ -14,6 +14,7 @@ import FirebaseDatabase
 class SettingsViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var SettingScrollView: UIScrollView!
     
     var imagePicker : UIImagePickerController?
     
